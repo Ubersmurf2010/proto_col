9863ce864c2b936fb5e04c8f6a9f04930163d473
Ubersmurf2010 <johndoe@example.com>
2024-01-28 23:24:55 +0300

    fix bugs v4

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -18,25 +18,27 @@ defineProps({
 <template> 
     <div>
         <div class="py-2 bg-zinc-300 relative bg-transparent border-rose-100 cursor-pointer border rounded-s transition py-1 hover:translate-y-2 hover:shadow-xl"> 
-            <img
+            <div class="flex justify-between"> 
+                <img
                 v-if="onClickFavourite"
                 :src="!isFavourite ? '/like-1.svg' : '/like-2.svg'" 
                 alt="Like1"
                 @click="onClickFavourite" 
                 class="absolute top-2 left-2"
             />
-            <img :src="imageurl" alt="Cells"/>
-            <div class="flex justify-between"> 
-                <p class="mt-1">{{ title }}: {{ price }}</p>
-                <div class="flex justify-between">
-                <img
-                    v-if="onClickAdd"
-                    @click="onClickAdd" 
-                    :src="!isAdded ? '/plus.svg' : '/checked.svg'" 
-                    alt="Plus"/>
-                </div>
-            </div>
             
+            <img
+                v-if="onClickAdd"
+                @click="onClickAdd" 
+                :src="!isAdded ? '/plus.svg' : '/checked.svg'" 
+                alt="Plus"
+                class="absolute top-20 left-20 h-14"/>
+
+            <img :src="imageurl" alt="Cells"/>
+            </div>
+            <div class="text-center"> 
+                <p class="mt-1">{{ title }}: {{ price }}</p>
+            </div>
         </div>
     </div>
 </template>
