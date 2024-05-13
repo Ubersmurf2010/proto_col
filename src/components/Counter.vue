34748d798802b123e1b5bc2008a1e8bbd282939f
Kazantsev Gera <georgiy.kazantsev.91@mail.ru>
2024-05-14 00:08:56 +0300

    add timer

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -18,6 +18,8 @@ export default {
     displayMiliSeconds: 0,
     loaded: false,
     expired: false,
+    upper: "Установлено время: год",
+    lower: "Выберите время и запустите таймер",
     startEnabled: true
   }),
 
@@ -43,8 +45,8 @@ export default {
   
   mounted() {
     console.log("Welcome onboard, captain!")
-    this.startTimer()
-    ditance.value = this.SetTimer(1, 0, 0, 0, 0, 0)
+    // this.startTimer()
+    // ditance.value = this.SetTimer(0, 0, 0, 0, 0, 0)
   },
   
   methods: {
@@ -86,7 +88,10 @@ export default {
       )
     },
     startTimer() {
+      this.expired = false
       this.startEnabled = false
+      this.upper ="Таймер запущен"
+      this.lower = ""
       try {
         this.showRemaining()  
         // get current date
@@ -115,9 +120,12 @@ export default {
     },
 
     setTimer() {
-      this.start = true
+      this.expired = false
       // set prepare time (1 minute)
-      ditance.value = this.SetTimer(0, 0, 0, 0, 1, 0)
+      ditance.value = this.SetTimer(0, 0, 0, 0, 60, 0)
+      this.upper ="Обед"
+      this.lower = ""
+      this.showRemainingOnce()
     },
 
     pauseTimer() {
@@ -125,16 +133,42 @@ export default {
       // get lastCall <> date
       lastCall.value = Date.now()
       clearInterval(timer.value)
+      this.upper ="Пауза"
+      this.lower = ""
     },
 
     restartTimer() {
+      this.expired = false
       ditance.value = this.SetPrepareTimer()
     },
     FiveMinuteTimer() {
-      ditance.value = this.SetTimer(0, 0, 0, 0, 5, 1)
+      ditance.value = this.SetTimer(0, 0, 0, 0, 5, 2)
+      this.upper ="Установлено время 5 минут"
+      this.lower = ""
+      this.showRemainingOnce()
     },
     TwoMinuteTimer() {
-      ditance.value = this.SetTimer(0, 0, 0, 0, 2, 1)
+      ditance.value = this.SetTimer(0, 0, 0, 0, 2, 2)
+      this.upper ="Установлено время 2 минуты"
+      this.lower = ""
+      this.showRemainingOnce()
+    },
+
+    showRemainingOnce() {
+      const now = new Date()
+      timerElapsed.value = ditance.value - now.getTime()
+      const days = Math.floor(timerElapsed.value / this._days)
+      const hours = Math.floor((timerElapsed.value % this._days) / this._hours)
+      const minutes = Math.floor((timerElapsed.value % this._hours) / this._minutes)
+      const seconds = Math.floor((timerElapsed.value % this._minutes) / this._seconds)
+      const miliseconds = Math.floor((timerElapsed.value % this._seconds) / this._miliseconds)
+
+      this.displayMinutes = this.formatNum(minutes)
+      this.displaySeconds = this.formatNum(seconds)
+      this.displayHours = this.formatNum(hours)
+      this.displayDays = this.formatNum(days)
+      this.displayMiliSeconds = this.formatNum(miliseconds)
+      this.loaded = true
     },
 
     showRemaining() {
@@ -158,8 +192,10 @@ export default {
         
 
         if (timerElapsed.value < 0) {
-          clearInterval(timer.value)
           this.expired = true
+          this.two = false
+          this.five = false
+
           return
         }
       }, 1000)
@@ -169,39 +205,39 @@ export default {
 </script>
 
 <template>
-  <h5 v-if="!startEnabled">Time has passed</h5>
   <div v-if="loaded">
+    <p class="text-5xl text-center">{{ upper }}</p>
+    <p class="text-5xl text-center" >{{ lower }}</p>
     <section
       class="text-3xl flex justify-center content-center flex-col mx-auto text-center"
     ></section>
-    <h5 v-if="expired">Time is over</h5>
-    <section class="flex text-6xl justify-center content-center">
+    <section v-if="!expired" class="flex text-6xl justify-center content-center">
       <div class="days mr-2 text-9xl relative">
         {{ displayDays }}
-        <div class="label text-sm absolute bottom-0">days</div>
+        <div class="label text-sm absolute bottom-0">дни</div>
       </div>
       <span class="leading-snug text-8xl">:</span>
       <div class="days mx-2 text-9xl relative">
         {{ displayHours }}
-        <div class="label text-sm absolute bottom-0">hour</div>
+        <div class="label text-sm absolute bottom-0">часы</div>
       </div>
       <span class="leading-snug text-8xl ">:</span>
 
       <div class="days mx-2 text-9xl relative">
         {{ displayMinutes }}
-        <div class="label text-sm absolute bottom-0">minutes</div>
+        <div class="label text-sm absolute bottom-0">минуты</div>
       </div>
       <span class="leading-snug text-8xl">:</span>
 
       <div class="days ml-2 text-9xl relative">
         {{ displaySeconds }}
-        <div class="label text-sm absolute bottom-0">seconds</div>
+        <div class="label text-sm absolute bottom-0">секунды</div>
       </div>
       <span class="leading-snug text-8xl">:</span>
 
       <div class="days ml-2 text-9xl relative">
         {{ displayMiliSeconds }}
-        <div class="label text-sm absolute bottom-0">miliseconds</div>
+        <div class="label text-sm absolute bottom-0">милисекунды</div>
       </div>
 
     </section>
@@ -210,22 +246,19 @@ export default {
     <button
       v-if="startEnabled"
       class="disabled:bg-slate-400 cursor pointer transition bg-lime-500 w-full rounded-xl my-3 py-3 text-white hover:bg-lime-600 active:bg-lime-700" 
-      @click="() => startTimer()">Start</button>
+      @click="() => startTimer()">Старт!</button>
+    <button
+      class="disabled:bg-slate-400 cursor pointer transition bg-sky-500 w-full rounded-xl my-3 py-3 text-white hover:bg-sky-600 active:bg-sky-700" 
+      @click="() => FiveMinuteTimer()">Установить 5 минут</button>
     <button
       class="disabled:bg-slate-400 cursor pointer transition bg-red-500 w-full rounded-xl my-3 py-3 text-white hover:bg-red-600 active:bg-red-700" 
-      @click="() => FiveMinuteTimer()">Set 5 min.</button>
-    <button
-      class="disabled:bg-slate-400 cursor pointer transition bg-red-500 w-full rounded-xl my-3 py-3 text-white hover:bg-red-600 active:bg-red-700" 
-      @click="() => TwoMinuteTimer()">Set 2 min.</button>
-    <button 
-      class="disabled:bg-slate-400 cursor pointer transition bg-blue-500 w-full rounded-xl my-3 py-3 text-white hover:bg-blue-600 active:bg-blue-700" 
-      @click="() => restartTimer()">Restart</button> 
+      @click="() => TwoMinuteTimer()">Установить 2 минуты</button>
     <button
       class="disabled:bg-slate-400 cursor pointer transition bg-yellow-500 w-full rounded-xl my-3 py-3 text-white hover:bg-yellow-600 active:bg-yellow-700" 
-      @click="() => pauseTimer()">Pause</button> 
+      @click="() => pauseTimer()">Пауза</button> 
     <button
-      class="disabled:bg-slate-400 cursor pointer transition bg-pink-500 w-full rounded-xl my-3 py-3 text-white hover:bg-pink-600 active:bg-pink-700" 
-      @click="() => setTimer()">Set</button>
+      class="disabled:bg-slate-400 cursor pointer transition bg-violet-500 w-full rounded-xl my-3 py-3 text-white hover:bg-violet-600 active:bg-violet-700" 
+      @click="() => setTimer()">Обед</button>
     <input type="text">
 
   </div>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -11,8 +11,8 @@ defineProps({
       <div class="flex items-center gap-4">
         <img src="/logo.jpeg" alt="Logo" class="w-10" />
         <div>
-          <h2 class="text-xl font-bold uppercase">Major League's utilityes</h2>
-          <p class="text-slate-500">beta test</p>
+          <h2 class="text-xl font-bold uppercase">Major League's</h2>
+          <p class="text-slate-500">V2.1</p>
         </div>
       </div>
     </router-link>
@@ -23,27 +23,28 @@ defineProps({
         class="flex items-center gap-3 cursor-pointer text-slate-500 hover:text-black"
       >
         <img class="w-8 h-8" src="/trasher.png" alt="Cart" />
-        <b> {{ totalPrice }} points</b>
+        <b> {{ totalPrice }} очков</b>
+        <span>Составить протокол</span>
       </li>
 
       <router-link to="/favourites">
         <li class="flex items-center gap-3 cursor-pointer text-slate-500 hover:text-black">
           <img src="/heart.svg" alt="Cart" />
-          <span>Controversial cells</span>
+          <span>Лучшие ячейки</span>
         </li>
       </router-link>
 
       <router-link to="/countdown">
         <li class="flex items-center gap-3 cursor-pointer text-slate-500 hover:text-black">
           <img src="/emoji-2.png" width="50" height="50" alt="Cart" />
-          <span>Timer</span>
+          <span>Таймер</span>
         </li>
       </router-link>
 
       <router-link to="/login">
         <li class="flex items-center gap-3 cursor-pointer text-slate-500 hover:text-black">
           <img src="/profile.svg" alt="Cart" />
-          <span> Login </span>
+          <span>Логин</span>
         </li>
       </router-link>
     </ul>
